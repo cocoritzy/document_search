18af5aeb203f94c9987276f663ee51e0f081ca1c
cocoritzy <lilounina.nina@gmail.com>
2025-04-21 18:13:08 +0000

    lundi soir

--- model.py
+++ model.py
@@ -3,11 +3,11 @@ import torch
 class CBOW(torch.nn.Module):
   def __init__(self, voc, emb):
     super().__init__()
-    self.emb = torch.nn.Embedding(num_embeddings=voc, embedding_dim=emb)
-    self.ffw = torch.nn.Linear(in_features=emb, out_features=voc, bias=False)
+    self.embeddings = torch.nn.Embedding(num_embeddings=voc, embedding_dim=emb)
+    self.linear = torch.nn.Linear(in_features=emb, out_features=voc)
 
   def forward(self, inpt):
-    emb = self.emb(inpt)
+    emb = self.embeddings(inpt)
     emb = emb.mean(dim=1)
-    out = self.ffw(emb)
+    out = self.linear(emb)
     return out
